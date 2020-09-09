7071effca0cbc4940abc3a4af45378a807e9a66a
LBU <lbu@LBUui-MacBook-Pro.local>
2020-09-09 15:55:23 -0400

    initial commit, successfully received data using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+
+
+
 
 function App() {
+  const [container, setContainer] = useState();
+  useEffect(() =>{
+
+
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        setContainer(res.data);
+      })
+      .catch(err => { 
+      debugger
+     })
+  }, [])
+
+
+
   return (
     <div className="App">
+      
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
